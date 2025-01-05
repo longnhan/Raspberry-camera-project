d183a4faf5bb55c62745fdfe717bef557d4c4cdd
nhan <longnhan252@gmail.com>
2025-01-05 18:51:14 +0700

    update build script

--- 02_source_code/tools/build.sh
+++ 02_source_code/tools/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Time build: $(date '+%Y-%m-%d %H:%M')"
+echo "User: $USER"
+
 # Define project root and build directory
 PROJECT_DIR=$(pwd)/..
 BUILD_DIR="${PROJECT_DIR}/build"
@@ -14,12 +17,12 @@ mkdir -p "$BUILD_DIR"
 
 # Run CMake to configure the project
 echo "Configuring the project using CMake..."
-cd "$BUILD_DIR" || exit 1  # Ensure the script stops if changing to the build directory fails
+cd "$BUILD_DIR" || exit 1
 cmake ..
 
 # Build the project
 echo "Building the project..."
-make -j$(nproc)  # Use all available CPU cores to speed up the build
+make -j$(nproc)
 
 # Check if build was successful
 if [ $? -eq 0 ]; then
@@ -28,3 +31,7 @@ else
     echo "Build failed. Please check the errors above."
     exit 1
 fi
+
+echo ::::----------------------------::::
+echo ::::-----------Finish-----------::::
+echo ::::----------------------------::::
