93429100cd707284f0e559809c2bf60f262129eb
nhan <longnhan252@gmail.com>
2025-01-05 16:45:40 +0700

    add cross compiler

---
+++ 02_source_code/tools/cross-pi-gcc-10.2.0-32/lib/gcc/arm-linux-gnueabihf/10.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../configure --prefix= --target=arm-linux-gnueabihf --enable-languages=c,c++,fortran --with-sysroot=/arm-linux-gnueabihf/libc --with-build-sysroot=/tmp/cross-pi-gcc-10.2.0-2/arm-linux-gnueabihf/libc --with-arch=armv8-a --with-fpu=neon-fp-armv8 --with-float=hard --disable-multilib";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm10e" }, { "arch", "armv8-a" }, { "float", "hard" }, { "fpu", "neon-fp-armv8" }, { "tls", "gnu" } };
