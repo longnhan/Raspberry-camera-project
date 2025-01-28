3144832cc6edb60d33ef0d9aef10f84970d54cbf
nhan <longnhan252@gmail.com>
2025-01-29 03:13:03 +0700

    add signal hanlder for main thread - signal handler SIGINT -  flag indicates running state

--- 02_source_code/app/main.cpp
+++ 02_source_code/app/main.cpp
@@ -2,6 +2,8 @@
 
 Button shutter_btn(SHUTTER_BUTTON, GPIO_INPUT, EN_ACTIVE_HIGH);
 
+std::atomic<bool> keep_running(true);
+
 void initializeModules() 
 {
 
@@ -9,15 +11,27 @@ void initializeModules()
 
 int main() 
 {
+    // Set up the signal handler
+    std::signal(SIGINT, signalHandler);
+
     // Initialize all modules
     initializeModules();
 
     std::cout << "Application started!" << std::endl;
     
-    while (true)
+    while (keep_running)
     {
 
     }
 
     return 0;
+}
+
+void signalHandler(int signal)
+{
+    if (signal == SIGINT)
+    {
+        std::cout << "\nCtrl+C detected. Cleaning up and exiting..." << std::endl;
+        keep_running = false;
+    }
 }
--- 02_source_code/app/main.h
+++ 02_source_code/app/main.h
@@ -2,9 +2,14 @@
 #define _MAIN_H_
 
 #include <iostream>
+#include <atomic>
+#include <csignal>
+#include <unistd.h>
 #include "camera_control.h"
 #include "button_handler.h"
 #include "gui_display.h"
 #include "log.h"
+
+void signalHandler(int signal);
 
 #endif /*_MAIN_H_*/
